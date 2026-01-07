03e7cdd1f402a650c5dbaf02f918b374e9055868
zikzani <zikzani@gmail.com>
2026-01-07 10:42:27 +0700

    edit union tambah kargo haji indonesia

--- custom_query_pendapatan.sql
+++ custom_query_pendapatan.sql
@@ -47,12 +47,11 @@ s.kota location_data_created__custom_field__nokprk,
 'AGRIPOS' transform__channel,
 'AGRIPOS' connote__connote_service,
 null connote_sender_custom_field__pks_no__to_be_verified,
---SUM(td.qty) AS total_qty,
 SUM(t.total) connote__connote_amount,
 COUNT(DISTINCT t.id) connote__connote_code,
 SUM(((p.price - p.base) * td.qty) / (1+(1.1/100))) pendapatan,
 SUM((p.price - p.base) * td.qty)
-- SUM(((p.price - p.base) * td.qty) / 1.011) pajak,
+- SUM(((p.price - p.base) * td.qty) / (1+(1.1/100))) pajak,
 0 fee_cod
 FROM agripost.transactions t
 LEFT JOIN agripost.transaction_details td
@@ -63,5 +62,23 @@ LEFT JOIN agripost.products p
 ON td.product_id = p.id
 WHERE t.store_id NOT IN (1,2,3,4,5,6,7,8,9,10,11,12)
 and UPPER(t.status) ='SELESAI'
+GROUP BY
+1,2,3,4,5,6,7
+union
+SELECT date(created_at)created_at ,
+'KARGO HAJI' customer_code,
+'KARGO HAJI' custom_field__jenis_barang,
+'KARGO HAJI' location_data_created__custom_field__nokprk,
+'KARGO HAJI'  transform__channel,
+'KARGO HAJI'  connote__connote_service,
+null connote_sender_custom_field__pks_no__to_be_verified,
+SUM(total_fee_idr)connote__connote_amount,
+COUNT(distinct no_resi)connote__connote_code,
+SUM(total_fee_idr)/(1+(1.1/100)) pendapatan,
+SUM(total_fee_idr)-(SUM(total_fee_idr)/(1+(1.1/100))) pajak,
+0 fee_cod
+FROM kargo.kargo_haji_kolekting
+where UPPER(status)='MANIFEST'
+and is_paid='t'
 GROUP BY
 1,2,3,4,5,6,7
